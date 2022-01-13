ae5209c0fae4176dcfb0f8673079252f51153728
Simon <simon.galet@gmail.com>
2022-01-13 22:03:14 +0100

    Add conference room and finish decoration

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,24 @@ import {bootstrapExtra} from "@workadventure/scripting-api-extra";
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
 bootstrapExtra().catch(e => console.error(e));
 
-let currentPopup: any = undefined;
-const today = new Date();
-const time = today.getHours() + ":" + today.getMinutes();
+const handleTextPopup = (zoneName: string, popupName: string, text: string) => {
+    let currentPopup: any = undefined;
 
-WA.room.onEnterZone('clock', () => {
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
-})
+    WA.room.onEnterZone(zoneName, () => {
+        currentPopup = WA.ui.openPopup(popupName, text, []);
+    })
 
-WA.room.onLeaveZone('clock', closePopUp)
+    WA.room.onLeaveZone(zoneName, closePopUp)
 
-function closePopUp(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
+    function closePopUp(){
+        if (currentPopup !== undefined) {
+            currentPopup.close();
+            currentPopup = undefined;
+        }
     }
-}
+};
+
+handleTextPopup("conferenceRoomPopupNameZone", "conferenceRoomPopupName", "Salle de conference (nom à déterminer)");
+handleTextPopup("meetingRoom1PopupNameZone", "meetingRoom1PopupName", "Salle de réunions 1  (nom à déterminer)");
+handleTextPopup("meetingRoom2PopupNameZone", "meetingRoom2PopupName", "Salle de réunions 2  (nom à déterminer)");
+handleTextPopup("meetingRoom3PopupNameZone", "meetingRoom3PopupName", "Salle de réunions 3  (nom à déterminer)");
