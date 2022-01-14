f6676a48b288e07fd018afdea0ab948031ee7697
Simon <simon.galet@gmail.com>
2022-01-14 19:28:40 +0100

    Update workadventure version

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,24 @@ import {bootstrapExtra} from "@workadventure/scripting-api-extra";
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
 bootstrapExtra().catch(e => console.error(e));
 
-const handleTextPopup = (zoneName: string, popupName: string, text: string) => {
+const handleTextPopup = (layerName: string, popupName: string, text: string) => {
     let currentPopup: any = undefined;
 
-    WA.room.onEnterZone(zoneName, () => {
+    WA.room.onEnterLayer(layerName).subscribe(() => {
         currentPopup = WA.ui.openPopup(popupName, text, []);
-    })
+    });
 
-    WA.room.onLeaveZone(zoneName, closePopUp)
-
-    function closePopUp(){
+    WA.room.onLeaveLayer(layerName).subscribe(() =>{
         if (currentPopup !== undefined) {
             currentPopup.close();
             currentPopup = undefined;
         }
-    }
+    });
 };
 
-handleTextPopup("conferenceRoomPopupNameZone", "conferenceRoomPopupName", "Salle de conference (nom à déterminer)");
-handleTextPopup("meetingRoom1PopupNameZone", "meetingRoom1PopupName", "Salle de réunions 1  (nom à déterminer)");
-handleTextPopup("meetingRoom2PopupNameZone", "meetingRoom2PopupName", "Salle de réunions 2  (nom à déterminer)");
-handleTextPopup("meetingRoom3PopupNameZone", "meetingRoom3PopupName", "Salle de réunions 3  (nom à déterminer)");
+handleTextPopup("zones/conferenceRoomPopupNameZone", "conferenceRoomPopupName", "Salle de conference (nom à déterminer)");
+handleTextPopup("zones/meetingRoom1PopupNameZone", "meetingRoom1PopupName", "Salle de réunions 1  (nom à déterminer)");
+handleTextPopup("zones/meetingRoom2PopupNameZone", "meetingRoom2PopupName", "Salle de réunions 2  (nom à déterminer)");
+handleTextPopup("zones/meetingRoom3PopupNameZone", "meetingRoom3PopupName", "Salle de réunions 3  (nom à déterminer)");
+
+WA.player.setOutlineColor(7, 55, 208);
