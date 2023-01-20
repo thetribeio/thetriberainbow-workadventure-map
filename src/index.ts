6f61275532610e7f8761dc5833dec5a227f96f58
JB-Doug <jbscana@gmail.com>
2023-01-20 14:33:55 +0100

    Pop OK, need to create a random number

--- src/index.ts
+++ src/index.ts
@@ -19,10 +19,28 @@ const handleTextPopup = (layerName: string, popupName: string, text: string) => 
         }
     });
 };
+const randomNumber: string = (Math.floor(Math.random() * (0 - 100 + 1))).toString();
+
+const handleNumberPopup = (layerName: string, popupName: string) => {
+    let currentPopup: any = undefined;
+
+
+    WA.room.onEnterLayer(layerName).subscribe(() => {
+        currentPopup = WA.ui.openPopup(popupName, randomNumber, []);
+    });
+
+    WA.room.onLeaveLayer(layerName).subscribe(() =>{
+        if (currentPopup !== undefined) {
+            currentPopup.close();
+            currentPopup = undefined;
+        }
+    });
+};
 
 handleTextPopup("zones/conferenceRoomPopupNameZone", "conferenceRoomPopupName", "Salle de conference (nom à déterminer)");
 handleTextPopup("zones/meetingRoom1PopupNameZone", "meetingRoom1PopupName", "Salle de réunions 1  (nom à déterminer)");
 handleTextPopup("zones/meetingRoom2PopupNameZone", "meetingRoom2PopupName", "Salle de réunions 2  (nom à déterminer)");
 handleTextPopup("zones/meetingRoom3PopupNameZone", "meetingRoom3PopupName", "Salle de réunions 3  (nom à déterminer)");
+handleNumberPopup("PopUpZones/testPopUpLayer", "testPopUp");
 
 WA.player.setOutlineColor(7, 55, 208);
